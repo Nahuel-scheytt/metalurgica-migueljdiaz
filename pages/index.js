9ad1b08c362eb88f2a7ccc45122d809a205c2ea7
Nahue Scheytt <nahuel.scheytt@gmail.com>
2020-07-30 23:58:13 -0300

    agregamos banner parallax

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Layout from '../components/layout'
+import { ParallaxBanner } from 'react-scroll-parallax';
 
 export default function Home() {
   return (
-    <Layout home />
+    <Layout home>
+      <ParallaxBanner
+        className="your-class"
+        layers={[
+          {
+            image: '../public/images/quienes-somos.jpg',
+            amount: 0.1,
+          },
+        ]}
+        style={{
+          height: '650px',
+        }}
+      >
+        <h1>Banner Children</h1>
+      </ParallaxBanner>
+    </Layout>
   )
 }
