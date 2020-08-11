e5246422f6c072457a35c1b4e4c4cc6412f269c5
Nahue Scheytt <nahuel.scheytt@gmail.com>
2020-08-10 21:23:16 -0300

    Banner Home - Add

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,10 @@
 import Layout from '../components/layout'
-import { ParallaxBanner } from 'react-scroll-parallax';
+import BannerHome from '../components/banner-home/index'
 
 export default function Home() {
   return (
     <Layout home>
-      <ParallaxBanner
-        className="your-class"
-        layers={[
-          {
-            image: '../public/images/quienes-somos.jpg',
-            amount: 0.1,
-          },
-        ]}
-        style={{
-          height: '650px',
-        }}
-      >
-        <h1>Banner Children</h1>
-      </ParallaxBanner>
+      <BannerHome />
     </Layout>
   )
 }
