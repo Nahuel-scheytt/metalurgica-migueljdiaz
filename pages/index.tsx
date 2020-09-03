000aa49d88d2e4c47b9ef69fd8080e0251569658
nescheytt <nahuel.scheytt@gmail.com>
2020-09-03 18:49:18 -0300

    Seccion nosotros (#8)
    * Home - Seccion nosotros
    
    * yarn
    
    * package npm
    
    * Eliminamos YARN del proyecto
    
    * Home - About componente - Cambiamos de directorio al componentr
    
    * Home - Componentes - Directorio correcto para los componentes de la Home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,12 @@
 import Layout from '../components/layout'
-import styled from 'styled-components';
-import { makeAbout, makeTitle, makeParagraph } from '../components/about/mixins'
-import BannerHome from '../components/banner-home/index'
-
-const About = styled.div`
-  ${() => makeAbout()}
-`
-const Title = styled.h3`
-  ${() => makeTitle()}
-`
-const Paragraph = styled.p`
-  ${() => makeParagraph()}
-`
+import BannerHome from '../components/pages-ui/home/banner-home/index'
+import About from '../components/pages-ui/home/about/index'
 
 const Home = () => {
   return (
     <Layout home>
       <BannerHome />
-
-      <About>
-        <Title>Nosotros</Title>
-        <Paragraph>Talleres Metalurgicos Miguel J Diaz es una empresa Argentina, dedicada a la fabricacion de herrajes para cortinas, surgida de un emprendimiento iniciado con el esfuerzo y trabajo sostenido desde 1970.</Paragraph>
-      </About>
+      <About />
     </Layout>
   )
 }
